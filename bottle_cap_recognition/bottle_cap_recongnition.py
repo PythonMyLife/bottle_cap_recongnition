a1bd4d4cdb9f97c99abc6c1107a18fb7e0b6d89e
LLL4040 <2293114926@qq.com>
2019-12-25 15:44:45 +0800

    添加侧面检测

--- bottle_cap_recognition/bottle_cap_recongnition.py
+++ bottle_cap_recognition/bottle_cap_recongnition.py
@@ -1,7 +1,9 @@
 # -*- coding: UTF-8 -*-
 from tkinter import Tk, Canvas, Button, filedialog, StringVar, Label
 from PIL import Image, ImageTk
-
+import numpy as np
+import imutils
+import cv2
 
 def resize(w_box, h_box, pil_image):
     w, h = pil_image.size  # 获取图像的原始大小
@@ -12,8 +14,26 @@ def resize(w_box, h_box, pil_image):
     height = int(h * factor)
     return pil_image.resize((width, height), Image.ANTIALIAS)
 
+# 创建形状检测类
+class ShapeDetector:
+	def __init__(self):
+		pass
 
-class bottle_cap_recognition:
+	def detect(self, c):
+		# 初始化形状名和近似的轮廓
+		shape = "unidentified"
+		peri = cv2.arcLength(c, True)
+		approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+		# 如果当前的轮廓含有4或5个顶点，则其为侧面
+		if len(approx) <= 5:
+			shape = "left"
+		# 否则的话，我们认为它是一个圆
+		else:
+			shape = "other"
+		# 返回形状的名称
+		return shape
+
+class BottleCapRecognition:
     def __init__(self, root):
         self.root = root
         self.root.title("瓶盖识别")
@@ -32,30 +52,29 @@ class bottle_cap_recognition:
 
         # 图像选择按钮
         self.button_choose = Button(self.root, text='选择图片', command=lambda: self.choose(self.canvas2))
-        self.canvas1.create_window(50, 10, anchor='nw', window=self.button_choose)
+        self.canvas1.create_window(80, 50, anchor='center', window=self.button_choose)
 
         # 正面检测按钮
         self.button_front = Button(self.root, text='正面检测', command=lambda: self.front())
-        self.canvas1.create_window(50, 50, anchor='nw', window=self.button_front)
+        self.canvas1.create_window(80, 100, anchor='center', window=self.button_front)
 
         # 侧面检测按钮
         self.button_profile = Button(self.root, text='侧面检测', command=lambda: self.profile())
-        self.canvas1.create_window(50, 90, anchor='nw', window=self.button_profile)
+        self.canvas1.create_window(80, 150, anchor='center', window=self.button_profile)
 
         # 背面检测按钮
         self.button_back = Button(self.root, text='背面检测', command=lambda: self.back())
-        self.canvas1.create_window(50, 130, anchor='nw', window=self.button_back)
+        self.canvas1.create_window(80, 200, anchor='center', window=self.button_back)
 
         # 形态检测按钮
         self.button_all = Button(self.root, text='形态检测', command=lambda: self.all())
-        self.canvas1.create_window(50, 170, anchor='nw', window=self.button_all)
+        self.canvas1.create_window(80, 250, anchor='center', window=self.button_all)
 
         # 输出语句
         self.output = StringVar()
         # 输出框
         self.text = Label(self.root, textvariable=self.output)
-        self.canvas1.create_window(10, 210, anchor='nw', window=self.text)
-
+        self.canvas1.create_window(80, 290, anchor='n', window=self.text)
 
 
     def choose(self, canvas2):
@@ -96,10 +115,50 @@ class bottle_cap_recognition:
         self.output.set(long_text)
 
     def profile(self):
-        img_input = self.photo
-        img_output = img_input
+        long_text = '侧面瓶盖的坐标为'
+        # 将PIL图像转化为opencv图像
+        img_cv = cv2.cvtColor(np.array(self.photo), cv2.COLOR_RGB2BGR)
+        # 进行裁剪操作
+        resized = imutils.resize(img_cv, width=300)
+        ratio = img_cv.shape[0] / float(resized.shape[0])
+        # 进行高斯模糊操作
+        blurred = cv2.GaussianBlur(resized, (5, 5), 0)
+        # 进行图片灰度化
+        gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
+        # 进行颜色空间的变换
+        lab = cv2.cvtColor(blurred, cv2.COLOR_BGR2LAB)
+        # 进行阈值分割
+        thresh = cv2.threshold(gray, 60, 255, cv2.THRESH_BINARY)[1]
+        # 在二值图片中寻找轮廓
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        # 初始化形状检测器和颜色标签
+        sd = ShapeDetector()
+        # 遍历每一个轮廓
+        for c in cnts:
+            # 计算每一个轮廓的中心点
+            M = cv2.moments(c)
+            if(M["m00"]==0): # this is a line
+                shape = "line" 
+                continue
+            cX = int((M["m10"] / M["m00"]) * ratio)
+            cY = int((M["m01"] / M["m00"]) * ratio)
+            # 进行颜色检测和形状检测
+            shape = sd.detect(c)
+            #color = cl.label(lab, c)
+            # 进行坐标变换
+            c = c.astype("float")
+            c *= ratio
+            c = c.astype("int")
+            text = "{}".format(shape)
+            if(text=="left"):
+                # 绘制轮廓并显示结果
+                cv2.drawContours(img_cv, [c], -1, (0, 0, 255), 2)
+                cv2.putText(img_cv, text, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                long_text += '\n[' + str(cX) + ',' + str(cY) + ']'
+        # 将opencv图像转化为PIL图像
+        img_output = Image.fromarray(cv2.cvtColor(img_cv,cv2.COLOR_BGR2RGB))
         self.show(img_output)
-        long_text = '侧面瓶盖的坐标为\n' + '[1,1]'
         self.output.set(long_text)
 
     def all(self):
@@ -111,5 +170,5 @@ class bottle_cap_recognition:
 
 if __name__ == "__main__":
     tk_root = Tk()
-    recognition = bottle_cap_recognition(tk_root)
+    recognition = BottleCapRecognition(tk_root)
     tk_root.mainloop()
